82b13df6c59daf9bfedc68b512e5e28c460540e0
IsaRampage <steven.cichowski@web.de>
2023-05-02 14:52:04 +0200

    Initial Commit

---
+++ TasksToday/TasksTodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksTodayApp.swift
+//  TasksToday
+//
+//  Created by Isa Rampage on 02.05.23.
+//
+
+import SwiftUI
+
+@main
+struct TasksTodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
